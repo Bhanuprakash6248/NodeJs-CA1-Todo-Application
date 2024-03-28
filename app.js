5d36abb4c6abd9dac65a5b281dc928aad5aacd94
Bhanuprakash6248 <131347468+Bhanuprakash6248@users.noreply.github.com>
2024-03-28 19:56:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const sqlite3 = require('sqlite3')
 
 const path = require('path')
 
-const addDate = require('date-fns')
+const format = require('date-fns/format')
+const isMatch = require('date-fns/isMatch')
+const isValid = require('date-fns/isValid')
+
 const app = express()
 app.use(express.json())
 
@@ -59,8 +62,8 @@ const hasPriorityProperty = requestQuery => {
 const hasStatusProperty = requestQuery => {
   return requestQuery.status !== undefined
 }
-const hasSearchProperty = requestQuery => {
-  return requestQuery.search_q !== undefined
+const hasDueDateProperty = requestQuery => {
+  return requestQuery.due_date !== undefined
 }
 
 const isValidatePriority = item => {
@@ -78,17 +81,27 @@ const isValidateStatus = item => {
   }
 }
 const isValidateCategory = item => {
-  if (item === ' WORK' || item === 'HOME' || item === 'LEARNING') {
+  if (item === 'WORK' || item === 'HOME' || item === 'LEARNING') {
     return true
   } else {
     return false
+  }
+}
+const convertCamelCase = dbObject => {
+  return {
+    id: dbObject.id,
+    todo: dbObject.todo,
+    priotity: dbObject.priority,
+    status: dbObject.status,
+    category: dbObject.category,
+    dueDate: dbObject.due_date,
   }
 }
 
 app.get('/todos/', async (request, response) => {
   let data = null
   let todoQuery = ''
-  const {search_q, priority, category, status} = request.query
+  const {search_q = '', priority, category, status} = request.query
   switch (true) {
     case hasPriorityAndStatusProperties(request.query):
       todoQuery = `
@@ -96,12 +109,12 @@ app.get('/todos/', async (request, response) => {
       FROM todo
       WHERE 
         todo LIKE '%${search_q}%'
-        AND prirority = '${priority}'
+        AND priority = '${priority}'
         AND status = '${status}';
       `
       if (isValidateStatus(status) && isValidatePriority(priority)) {
         data = await db.all(todoQuery)
-        response.send(data)
+        response.send(data.map(each => convertCamelCase(each)))
       } else if (isValidatePriority(priority)) {
         response.status(400)
         response.send('Invalid Todo status')
@@ -116,12 +129,12 @@ app.get('/todos/', async (request, response) => {
       FROM todo
       WHERE 
         todo LIKE '%${search_q}%'
-        AND prirority = '${priority}'
+        AND priority = '${priority}'
         AND category = '${category}';       
       `
       if (isValidateCategory(category) && isValidatePriority(priority)) {
         data = await db.all(todoQuery)
-        response.send(data)
+        response.send(data.map(each => convertCamelCase(each)))
       } else if (isValidatePriority(priority)) {
         response.status(400)
         response.send('Invalid Todo Category')
@@ -141,7 +154,7 @@ app.get('/todos/', async (request, response) => {
       `
       if (isValidateCategory(category) && isValidateStatus(status)) {
         data = await db.all(todoQuery)
-        response.send(data)
+        response.send(data.map(each => convertCamelCase(each)))
       } else if (isValidateCategory(category)) {
         response.status(400)
         response.send('Invalid Todo Status')
@@ -160,7 +173,7 @@ app.get('/todos/', async (request, response) => {
       `
       if (isValidateCategory(category)) {
         data = await db.all(todoQuery)
-        response.send(data)
+        response.send(data.map(each => convertCamelCase(each)))
       } else {
         response.status(400)
         response.send('Invalid Todo Category')
@@ -176,7 +189,7 @@ app.get('/todos/', async (request, response) => {
       `
       if (isValidatePriority(priority)) {
         data = await db.all(todoQuery)
-        response.send(data)
+        response.send(data.map(each => convertCamelCase(each)))
       } else {
         response.status(400)
         response.send('Invalid Todo Priority')
@@ -192,7 +205,7 @@ app.get('/todos/', async (request, response) => {
       `
       if (isValidateStatus(status)) {
         data = await db.all(todoQuery)
-        response.send(data)
+        response.send(data.map(each => convertCamelCase(each)))
       } else {
         response.status(400)
         response.send('Invalid Todo Status')
@@ -206,6 +219,130 @@ app.get('/todos/', async (request, response) => {
         todo LIKE '%${search_q}%';       
       `
       data = await db.all(todoQuery)
-      response.send(data)
+      response.send(data.map(each => convertCamelCase(each)))
+  }
+})
+
+//API -2
+
+app.get('/todos/:todoId/', async (request, response) => {
+  const {todoId} = request.params
+  const selectedQuery = `
+  SELECT *
+  FROM todo
+  WHERE id = ${todoId};
+  `
+  const dbResponse = await db.get(selectedQuery)
+  response.send(convertCamelCase(dbResponse))
+})
+
+//API - 3
+app.get('/agenda/', async (request, response) => {
+  const {date} = request.query
+  console.log(isMatch(date, 'yyyy-MM-dd'))
+  if (date === undefined) {
+    response.status(400)
+    response.send('Invalid Due Date')
+  } else {
+    if (hasDueDateProperty(request.query)) {
+      const newDate = format(new Date(date), 'yyyy-MM-dd')
+      console.log(newDate)
+      const getQuery = `
+      SELECT *
+      FROM todo
+      WHERE 
+      due_date = "${newDate}";
+      `
+      const data = await db.all(getQuery)
+      response.send(data.map(each => convertCamelCase(each)))
+    } else {
+      response.status(400)
+      response.send('Invalid Due Date')
+    }
+  }
+})
+
+//API-4
+app.post('/todos/', async (request, response) => {
+  const {id, todo, priority, status, category, dueDate} = request.body
+  const newDate = format(new Date(dueDate), 'yyyy-MM-dd')
+  const todoQueryInsert = `
+  INSERT INTO 
+  todo(id,todo,priority,status,category,due_date)
+  VALUES (
+    ${id},
+    '${todo}',
+    '${priority}',
+    '${status}',
+    '${category}',
+    '${newDate}',  
+  );
+  `
+  await db.run(todoQueryInsert)
+  response.send('Todo Successfully Added')
+})
+
+//API-5 --> Update todo of specified ID
+app.put('/todos/:todoId/', async (request, response) => {
+  const {todoId} = request.params
+  const {status, priority, todo, category, dueDate} = request.body
+  const updatedQuery = ''
+  switch (true) {
+    case status !== undefined:
+      updatedQuery = `
+      UPDATE todo
+      SET
+        status = "${status}"
+      WHERE
+        todoId = ${todoId};
+      `
+      await db.run(updatedQuery)
+      response.send('Status Updated')
+      break
+    case priority !== undefined:
+      updatedQuery = `
+      UPDATE todo
+      SET
+        priority = "${priority}"
+      WHERE
+        todoId = ${todoId};
+      `
+      await db.run(updatedQuery)
+      response.send('Priority Updated')
+      break
+    case todo !== undefined:
+      updatedQuery = `
+      UPDATE todo
+      SET
+        todo = "${todo}"
+      WHERE
+        todoId = ${todoId};
+      `
+      await db.run(updatedQuery)
+      response.send('Todo Updated')
+      break
+    case category !== undefined:
+      updatedQuery = `
+      UPDATE todo
+      SET
+        category = "${category}"
+      WHERE
+        todoId = ${todoId};
+      `
+      await db.run(updatedQuery)
+      response.send('Category Updated')
+      break
+
+    case dueDate !== undefined:
+      updatedQuery = `
+      UPDATE todo
+      SET
+        due_date = "${dueDate}"
+      WHERE
+        todoId = ${todoId};
+      `
+      await db.run(updatedQuery)
+      response.send('Due Date Updated')
+      break
   }
 })
